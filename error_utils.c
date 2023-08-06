0d45171905f3486920b787461ff234b5c4e89f5e
AhmedAlfahal <ahmednader02@gmail.com>
2023-08-06 19:49:23 -0400

    starting free utils

--- error_utils.c
+++ error_utils.c
@@ -11,3 +11,22 @@ void	closing_and_freeing(int fd, char *line, int ext)
 		exit(1);
 	}
 }
+
+void	free_2d_array(char **s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		free(s[i++]);
+	free(s[i]);
+	free(s);
+}
+
+void	free_all(t_DES *d)
+{
+	for (int i = 0; i < 13; i++)
+	{
+		
+	}
+}
--- free_utils.c
+++
@@ -1,2 +0,0 @@
-#include "DES.h"
-
